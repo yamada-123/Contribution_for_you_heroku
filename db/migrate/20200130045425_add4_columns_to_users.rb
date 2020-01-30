90ba34456879d2408daf8de09f9a129f8e653025
satoshi yamada <yasain0852@yahoo.co.jp>
2020-01-30 13:56:22 +0900

    herokuデプロイ

---
+++ db/migrate/20200130045425_add4_columns_to_users.rb
@@ -0,0 +1,8 @@
+class Add4ColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :picture, :string
+    add_column :users, :introduction, :text
+    add_column :users, :region, :string
+  end
+end
