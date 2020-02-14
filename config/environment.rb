0be91e6f4b29b7f5be36f29b19ec79dd0334d6fc
satoshi yamada <yasain0852@yahoo.co.jp>
2020-02-14 17:34:56 +0900

    heroku

--- config/environment.rb
+++ config/environment.rb
@@ -6,12 +6,12 @@ Rails.application.initialize!
 
 # config.action_mailer.default_url_options = { host: 'https://mighty-stream-12227.herokuapp.com/' }
 # ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#   :address        => 'smtp.sendgrid.net',
+#   :port           => '587',
+#   :authentication => :plain,
+#   :user_name      => ENV['SENDGRID_USERNAME'],
+#   :password       => ENV['SENDGRID_PASSWORD'],
+#   :domain         => 'heroku.com',
+#   :enable_starttls_auto => true
+# }
