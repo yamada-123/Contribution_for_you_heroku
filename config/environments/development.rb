2eb5693fc5e70af1534b4cd3bd3e06cf9dde9848
satoshi yamada <yasain0852@yahoo.co.jp>
2020-02-14 14:46:11 +0900

    heroku修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
 
-  host = 'localhost'
-  Rails.application.routes.default_url_options[:host] = host
+  # host = 'localhost'
+  # Rails.application.routes.default_url_options[:host] = host
 end
