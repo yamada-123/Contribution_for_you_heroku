760083a26e83ae6e2d0743a673e02270d638a924
satoshi yamada <yasain0852@yahoo.co.jp>
2020-02-14 15:23:16 +0900

    heroku

--- config/environment.rb
+++ config/environment.rb
@@ -7,11 +7,11 @@ Rails.application.initialize!
 # config.action_mailer.default_url_options = { host: 'https://mighty-stream-12227.herokuapp.com/' }
 # ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  domain: "heroku.com",
-  address: "smtp.SendGrid.net",
-  port: 587,
-  authentication: :plain,
-  enable_starttls_auto: true
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,17 +93,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://mighty-stream-12227.herokuapp.com/' }
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
-    address: "smtp.SendGrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.default_url_options = { host: 'https://mighty-stream-12227.herokuapp.com/' }
+  # ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.smtp_settings = {
+  #   user_name: ENV['SENDGRID_USERNAME'],
+  #   password: ENV['SENDGRID_PASSWORD'],
+  #   domain: "heroku.com",
+  #   address: "smtp.SendGrid.net",
+  #   port: 587,
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
   # host = 'localhost'
   # Rails.application.routes.default_url_options[:host] = host
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
