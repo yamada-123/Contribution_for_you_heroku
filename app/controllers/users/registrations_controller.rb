c209b34774f435d286badbdbd9460ad9e9565eb1
satoshi yamada <yasain0852@yahoo.co.jp>
2020-02-14 18:50:07 +0900

    heroku

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -55,7 +55,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    demands_path
+    supplies_path
   end
 
   # The path used after sign up for inactive accounts.
@@ -64,7 +64,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   def after_inactive_sign_up_path_for(resource)
-    demands_path                   
+    supplies_path                   
   end
 
   protected
