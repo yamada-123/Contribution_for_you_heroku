15c43fa26a26a37ee1e18c6cc776bb705f532fa6
satoshi yamada <yasain0852@yahoo.co.jp>
2020-02-14 16:45:21 +0900

    heroku

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module Contribution
         request_specs: false
     end
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
