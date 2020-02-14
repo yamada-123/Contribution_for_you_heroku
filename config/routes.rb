30fe6d3a5731695c424908bf04c6da9c29f5d272
satoshi yamada <yasain0852@yahoo.co.jp>
2020-02-14 17:47:37 +0900

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
    }
-   devise_for :users, :controllers => {
-     :registrations => 'registrations'
-    }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'demands#index' #deviseでログインした後のページへ飛ぶ
 
