ad8652d52a3fd0ece90ec347e4230aabb5d06a10
satoshi yamada <yasain0852@yahoo.co.jp>
2020-02-15 11:11:24 +0900

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
 
-  config.action_mailer.default_url_options = { :host => 'mighty-stream-12227.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'damp-reef-84608.herokuapp.com' }
   
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
