0691eaa237ed6345c824e85a065148ab8bd2f2ac
satoshi yamada <yasain0852@yahoo.co.jp>
2020-02-27 12:07:00 +0900

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,28 +43,57 @@ Rails.application.configure do
   
   
   
-  config.action_mailer.default_url_options = { host: 'intense-inlet-32914.herokuapp.com' }
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
-    address: "smtp.SendGrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.default_url_options = { host: 'intense-inlet-32914.herokuapp.com' }
+  # ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.smtp_settings = {
+  #   user_name: ENV['SENDGRID_USERNAME'],
+  #   password: ENV['SENDGRID_PASSWORD'],
+  #   domain: "heroku.com",
+  #   address: "smtp.SendGrid.net",
+  #   port: 587,
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
   
-  config.action_mailer.default_url_options = { host: '18.180.34.62' }
+  # config.action_mailer.default_url_options = { host: '18.180.34.62' }
+  # ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.smtp_settings = {
+  #   user_name: ENV['SENDGRID_USERNAME'],
+  #   password: ENV['SENDGRID_PASSWORD'],
+  #   domain: "18.180.34.62",
+  #   address: "smtp.SendGrid.net",
+  #   port: 587,
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
+
+
+  config.action_mailer.default_url_options = 
+    ENV['HEROKU'] ? 
+      { host: 'intense-inlet-32914.herokuapp.com' } :
+      { host: '18.180.34.62' }
+  
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: "18.180.34.62",
-    address: "smtp.SendGrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  ActionMailer::Base.smtp_settings = 
+  ENV['HEROKU'] ? 
+    {
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      domain: "heroku.com",
+      address: "smtp.SendGrid.net",
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
+    } :
+    {
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      domain: "18.180.34.62",
+      address: "smtp.SendGrid.net",
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+
   
 end
